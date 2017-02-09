746deb8d18393be39dd589df8dcb9e560a7525f2
fiveinfinity <jlachance1@gmail.com>
2017-02-08 20:45:49 -0600

    fixed media queries

--- js/index.js
+++ js/index.js
@@ -1,106 +1,29 @@
+var ids = ['#welcome', '#products', '#about', '#privacy', '#terms', '#returns'];
+
 $(document).ready(function(){
-	$('#products').hide();
-	$('#about').hide();
-	$('#privacy').hide();
-	$('#terms').hide();
-	$('#returns').hide();
-	$('#hr-top').hide();
-	$('#hr-bottom').hide();
+	// Doesn't hide welcome page
+	for (var i = 1; i < ids.length; i++) {
+       $(ids[i]).hide();
+  }
 
-	$('#welcome-link').click(function(e) {
-			e.preventDefault();
-			var active = getActiveDiv();
+	$('*[id*=-link]').on('click', function(e) {
+		e.preventDefault();
+		var target = '#' + $(e.currentTarget).attr('id').slice(0, -5);
+		var active = '#' + $('.jumbotron .active').attr('id');
 
-				$(active).fadeOut('slow', function() {
-					$('#welcome').fadeIn('slow').addClass('active');
-				}).removeClass('active');
+			$(active).fadeOut('slow', function() {
+				$(target).fadeIn('slow').addClass('active');
+			}).removeClass('active');
 
-				//If coming from PRODUCTS page, HR's need to be removed
+			//If coming from PRODUCTS page, HR's need to be removed
+			if (i === '#welcome' || i === '#about') {
 				$('#hr-top').fadeOut('slow');
 				$('#hr-bottom').fadeOut('slow');
-	});
-
-	$('#products-link').click(function(e) {
-			e.preventDefault();
-			var active = getActiveDiv();
-
-				$(active).fadeOut('slow', function() {
-					$('#products').fadeIn('slow').addClass('active');
-				}).removeClass('active');
-
+			} else {
 				if ($(window).width() >= 700) {
 					$('#hr-top').fadeIn('slow');
 					$('#hr-bottom').fadeIn('slow');
 				}
-	});
-
-	$('#about-link').click(function(e) {
-			e.preventDefault();
-			var active = getActiveDiv();
-
-				$(active).fadeOut('slow', function() {
-					$('#about').fadeIn('slow').addClass('active');
-				}).removeClass('active');
-
-				//If coming from PRODUCTS page, HR's need to be removed
-				$('#hr-top').fadeOut('slow');
-				$('#hr-bottom').fadeOut('slow');
-	});
-	$('#privacy-link').click(function(e) {
-			e.preventDefault();
-			var active = getActiveDiv();
-
-				$(active).fadeOut('slow', function() {
-					$('#privacy').fadeIn('slow').addClass('active');
-				}).removeClass('active');
-
-				if ($(window).width() >= 700) {
-					$('#hr-top').fadeIn('slow');
-					$('#hr-bottom').fadeIn('slow');
-				}
-	});
-	$('#terms-link').click(function(e) {
-			e.preventDefault();
-			var active = getActiveDiv();
-
-				$(active).fadeOut('slow', function() {
-					$('#terms').fadeIn('slow').addClass('active');
-				}).removeClass('active');
-
-				if ($(window).width() >= 700) {
-					$('#hr-top').fadeIn('slow');
-					$('#hr-bottom').fadeIn('slow');
-				}
-	});
-	$('#returns-link').click(function(e) {
-			e.preventDefault();
-			var active = getActiveDiv();
-
-				$(active).fadeOut('slow', function() {
-					$('#returns').fadeIn('slow').addClass('active');
-				}).removeClass('active');
-
-				if ($(window).width() >= 700) {
-					$('#hr-top').fadeIn('slow');
-					$('#hr-bottom').fadeIn('slow');
-				}
+			}
 	});
 });
-
-function getActiveDiv() {
-	if ($('#welcome').hasClass('active')) {
-		return '#welcome';
-	} else if ($('#products').hasClass('active')) {
-		return '#products';
-	} else if ($('#about').hasClass('active')) {
-		return '#about';
-	} else if ($('#privacy').hasClass('active')) {
-		return '#privacy';
-	} else if ($('#terms').hasClass('active')) {
-		return '#terms';
-	} else if ($('#returns').hasClass('active')) {
-		return '#returns';
-	} else {
-		console.log('no active class! Error!');
-	}
-}
